a054bbe333a0c9b6287b010ae5cc056231dfd195
lichen <1176343828@qq.com>
2022-11-19 17:56:58 +0800

    数组指针测试

--- main.c
+++ main.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 
-void switch_sums(int* a, int* b)
-{
-    int tmp;
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
 int main() {
-    int a = 10;
-    int b = 20;
-    printf("交换前a = %d\tb = %d\n", a, b);
-    switch_sums(&a, &b);
-    printf("交换后a = %d\tb = %d\n", a, b);
+    int arr[3] ={1,2,3};
+    int *p = arr;
+    //printf("%d\t", *arr++); //常量指针不允许增加
+    printf("%d\t", *p++);
+    printf("%d\n", *p);
+    printf("int* size:%d\n", sizeof(p));
     return 0;
 }
