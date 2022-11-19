cf91dc06ab851ef0f34c486965db426561c8938c
lichen <1176343828@qq.com>
2022-11-19 16:24:35 +0800

    数字交换

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+void switch_sums(int* a, int* b)
+{
+    int tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
 int main() {
-    printf("Hello, World!\n");
+    int a = 10;
+    int b = 20;
+    printf("交换前a = %d\tb = %d\n", a, b);
+    switch_sums(&a, &b);
+    printf("交换后a = %d\tb = %d\n", a, b);
     return 0;
 }
