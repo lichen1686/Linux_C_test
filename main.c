aa11fce49fd065042495f393db2b553d0eaea931
lichen <1176343828@qq.com>
2022-11-21 00:16:53 +0800

    函数嵌套测试

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int __attribute__((__cdecl__)) getMaxFromTwo(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+int __attribute__((__cdecl__)) getMaxNumFromFour(int a, int b, int c, int d)
+{
+    int max;
+    max = getMaxFromTwo(a, b);
+    max = getMaxFromTwo(max, c);
+    max = getMaxFromTwo(max, d);
+    return max;
+}
+
+int main()
+{
+    int data1, data2, data3, data4, maxNum;
+    puts("Please input four numbers: ");
+    scanf("%d%d%d%d", &data1, &data2, &data3, &data4);
+
+    maxNum = getMaxNumFromFour(data1, data2, data3, data4);
+
+    printf("The biggest nums: %d", maxNum);
+
 }
