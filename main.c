3c1df5b22c12dcb906a846b2b3063c18bf93612a
lichen <1176343828@qq.com>
2022-12-04 22:31:40 +0800

    指针大小测试

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int main() {
     //printf("%d\t", *arr++); //常量指针不允许增加
     printf("%d\t", *p++);
     printf("%d\n", *p);
-    printf("int* size:%d\n", sizeof(p));
+    printf("arr size:%d\n", sizeof(arr));
+    printf("* p size:%d\n", sizeof(p));
     return 0;
 }
